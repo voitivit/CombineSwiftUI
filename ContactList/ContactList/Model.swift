60409ff9e1ba9ce95f2cc24546ba8a29939d41dd
voitivit <kurbanov_emil@icloud.com>
2022-04-13 22:10:58 +0300

    lesson7

---
+++ ContactList/ContactList/Model.swift
@@ -0,0 +1,75 @@
+//
+//  Model.swift
+//  ContactList
+//
+//  Created by emil kurbanov on 13.04.2022.
+//
+
+import Foundation
+
+struct User: Codable {
+    let id: Int
+    let name: String
+    let username: String
+    let email: String
+    let address: Address?
+    let phone: String
+    let website: String
+    let company: Company?
+    
+}
+
+struct Address: Codable {
+    let street: String
+    let suite: String
+    let city: String
+    let zipcode: String
+    let geo: Geo?
+}
+
+struct Geo: Codable {
+    let latitude: String
+    let longitude: String
+    
+    enum CodingKeys: String, CodingKey {
+        case latitude = "lat"
+        case longitude = "lng"
+    }
+}
+
+struct Company: Codable {
+    let name: String
+    let catchPhrase: String
+    let bs: String
+}
+
+
+
+
+
+
+
+
+  //  {
+    //    "id": 1,
+    //    "name": "Leanne Graham",
+    //    "username": "Bret",
+    //    "email": "Sincere@april.biz",
+    //    "address": {
+    //      "street": "Kulas Light",
+    //      "suite": "Apt. 556",
+    //      "city": "Gwenborough",
+    //      "zipcode": "92998-3874",
+    //      "geo": {
+    //        "lat": "-37.3159",
+    //        "lng": "81.1496"
+    //      }
+    //    },
+    //    "phone": "1-770-736-8031 x56442",
+    //    "website": "hildegard.org",
+    //    "company": {
+    //      "name": "Romaguera-Crona",
+    //      "catchPhrase": "Multi-layered client-server neural-net",
+    //      "bs": "harness real-time e-markets"
+    //    }
+    //  },
